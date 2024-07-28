ed528e6b995dd77641c1e2f2e448cecbca06eae7
Dwcks Dinh <corclan19@gmail.com>
2024-07-28 15:57:52 +0700

    fix something but still bug

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,13 +17,13 @@ return {
   },
 
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server",
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
         "stylua",
-  			"html-lsp",
-        "css-lsp" ,
+        -- "html-lsp",
+        -- "css-lsp" ,
         "prettier",
         "tailwindcss-language-server",
         "typescript-language-server",
@@ -31,8 +31,8 @@ return {
         "gofumpt",
         "goimports",
         "golines"
-  		},
-  	},
+      },
+    },
   },
   {
     "nvimtools/none-ls.nvim",
@@ -41,31 +41,30 @@ return {
     end,
   },
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
   },
   -- some bug in here, i'll fix that later
   {
     "windwp/nvim-ts-autotag",
-    config = function()
-      require('nvim-ts-autotag').setup({
-        aliases = {
-          ["html"] = "html",
-        },
-          filetypes = {
-            "html",
-            "xml",
-            "javascript",
-            "typescript",
-            "javascriptreact",
-            "typescriptreact",
-          }
-      })
-    end,
-  }
+    opts = {
+      -- Defaults
+      enable_close = true,         -- Auto close tags
+      enable_rename = true,        -- Auto rename pairs of tags
+      enable_close_on_slash = true -- Auto close on trailing </
+    },
+    -- Also override individual filetype configs, these take priority.
+    -- Empty by default, useful if one of the "opts" global settings
+    -- doesn't work well in a specific filetype
+    per_filetype = {
+      ["html"] = {
+        enable_close = false
+      },
+    },
+  },
 }
